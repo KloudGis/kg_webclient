7993caabb32406b3ef48b19412826ab96d7569ed
Jean-Félix Girard <jeanfelixg@gmail.com>
2011-11-18 09:06:43 -0500

    fix duplicate events on iOS

--- sc2/kloudgis/lib/app/lib/views/button.js
+++ sc2/kloudgis/lib/app/lib/views/button.js
@@ -4,22 +4,22 @@
 var get = SC.get;
 KG.Button = SC.Button.extend({
 
-	attributeBindings: ['type', 'disabled', 'title'],
+    attributeBindings: ['type', 'disabled', 'title'],
 
     label_loc: function() {
         return this.get('label').loc();
     }.property('label'),
 
     mouseUp: function(e) {
-		this._super(e);
+        this._super(e);
         var action = get(this, 'sc_action')
         if (action && KG.statechart) {
             KG.statechart.sendAction(action, this.get('content') || this.getPath('itemView.content'));
         }
-		if (e.stopPropagation) {
+        if (e.stopPropagation) {
             e.stopPropagation();
         }
-		return NO;
+        return NO;
     },
 
     keyUp: function(e) {
@@ -29,6 +29,12 @@ KG.Button = SC.Button.extend({
                 KG.statechart.sendAction(action, this.get('content') || this.getPath('itemView.content'));
             }
         }
-		return NO;
-    }
+        return NO;
+    },
+
+	//SC2 beta3- Bug with touch events => Duplicate events on touch device: One for the touch and one for mouse.  
+	//desactivate the touch specifics events
+    touchStart: function(touch) {},
+
+    touchEnd: function(touch) {}
 });
