59d7fd1e76f0c5b21a3d546cfd13e0514beb0f41
Jean-Félix Girard <jeanfelixg@gmail.com>
2011-11-18 07:55:18 -0500

    tweaks to hide the address bar on the iphone

--- sc2/kloudgis/lib/sandbox/lib/core_sandbox.js
+++ sc2/kloudgis/lib/sandbox/lib/core_sandbox.js
@@ -159,4 +159,12 @@ KG.core_sandbox = SC.Object.create({
 
 $(document).ready(function() {
     KG.statechart.initStatechart();
+    if ($.browser.isIphone) {
+		//tweaks to hide the address bar
+        $('#box').addClass('mobile');
+        setTimeout(function() {
+            window.scrollTo(0, 1);
+        },
+        1000);
+    }
 });
