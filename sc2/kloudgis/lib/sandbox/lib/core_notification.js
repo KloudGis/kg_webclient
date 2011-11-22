d6897efc9afe88a2fe19dd8e7a3685e6415619b7
Jean-Felix Girard <jeanfelixg@gmail.com>
2011-11-22 14:49:23 -0500

    use the general topic for notification.  Minor css change.

--- sc2/kloudgis/lib/sandbox/lib/core_notification.js
+++ sc2/kloudgis/lib/sandbox/lib/core_notification.js
@@ -7,7 +7,7 @@ KG.core_notification = SC.Object.create({
 
     listen: function() {
         var sandbox = KG.get('activeSandboxKey');
-        var location = '/api_notification/%@'.fmt(sandbox);
+        var location = '/api_notification/%@/general'.fmt(sandbox);
         //close active if any to avoid multiple open stream
         this.stopListen();
         $.atmosphere.subscribe(location, !this.callbackAdded ? this.atmosphereCallback: null, $.atmosphere.request = {
@@ -74,7 +74,7 @@ KG.core_notification = SC.Object.create({
             return NO;
         }
         var sandbox = KG.get('activeSandboxKey');
-        var location = '/api_notification/%@'.fmt(sandbox);
+        var location = '/api_notification/%@/general'.fmt(sandbox);
         this.connectedEndpoint.push(location, null, $.atmosphere.request = {
             data: JSON.stringify(message.toDataHash()),
             headers: KG.core_auth.createAjaxRequestHeaders()
